e8353b8d81b2169774020c13d04cec36f37b1bb8
linzhiyuan00 <2032247695@qq.com>
2019-05-27 13:14:22 +0800

    5-27

--- components/classify1/classify1.js
+++ components/classify1/classify1.js
@@ -11,6 +11,48 @@ Component({
    * 组件的初始数据
    */
   data: {
+    goodsList: [
+      {
+        big: true,
+        imgurl: 'https://i1.mifile.cn/a4/xmad_15586661012003_thOfZ.jpg',
+        title: '小米9 闪电行动',
+        desc: '6GB + 12GB版本 ',
+        price: '$2999',
+        buy: '立即购买'
+      },
+      {
+        big: false,
+        imgurl: 'https://i1.mifile.cn/a1/pms_1550642182.7527088!220x220.jpg',
+        title: '小米9 闪电行动',
+        desc: '6GB + 12GB版本 ',
+        price: '$2999',
+        buy: '立即购买'
+      },
+      {
+        big: false,
+        imgurl: 'https://i1.mifile.cn/a1/pms_1550642182.7527088!220x220.jpg',
+        title: '小米9 闪电行动',
+        desc: '6GB + 12GB版本 ',
+        price: '$2999',
+        buy: '立即购买'
+      },
+      {
+        big: false,
+        imgurl: 'https://i1.mifile.cn/a1/pms_1550642182.7527088!220x220.jpg',
+        title: '小米9 闪电行动',
+        desc: '6GB + 12GB版本 ',
+        price: '$2999',
+        buy: '立即购买'
+      },
+      {
+        big: false,
+        imgurl: 'https://i1.mifile.cn/a1/pms_1550642182.7527088!220x220.jpg',
+        title: '小米9 闪电行动',
+        desc: '6GB + 12GB版本 ',
+        price: '$2999',
+        buy: '立即购买'
+      }
+    ]
 
   },
 
