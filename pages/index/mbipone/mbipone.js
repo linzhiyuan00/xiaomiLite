26639e1c89ac86f7bb7b85ed0746a0cc4983c634
linzhiyuan00 <2032247695@qq.com>
2019-05-28 23:25:44 +0800

    5-28
    修改了大部分数据

--- pages/index/mbipone/mbipone.js
+++ pages/index/mbipone/mbipone.js
@@ -8,30 +8,30 @@ Page({
     interval: 3000,
     duration: 800,
     imgUrls: [
-      '/images/index/mbipone/0xiaomi9.png',
-      '/images/index/mbipone/0redmin7.jpg',
-      '/images/index/mbipone/0redmik20.jpg'
+      'https://i1.mifile.cn/a4/xmad_15587134858173_vcaQD.jpg',
+      'https://i1.mifile.cn/a4/xmad_15529884424562_HKYaG.jpg',
+      'https://i8.mifile.cn/b2c-mimall-media/1f60a0afff47ac32b7a1659e2b24c19a.jpg?w=1708&h=868'
     ],
     forrecmdgoods1: [
       {
-        url1: '/images/index/mbipone/heisa2.png',
+        url1: 'https://i1.mifile.cn/a1/pms_1552805539.44635255!200x200.jpg',
         goodsname1: '黑鲨游戏手机 2',
         goodsspecial1: '晓龙855，立体触控',
         goodsprice1: '2899元起',
-        url2: '/images/index/mbipone/xiaomi9SE.png',
+        url2: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/2eebb2092d4fa5f1ff0576881d112f67.jpg?w=1080&h=1080',
         goodsname2: '小米9 SE',
         goodsspecial2: '索尼4800万三摄，晓龙712',
         goodsprice2: '1999元起',
       },
       {
-        url1: '/images/index/mbipone/heisa2.png',
-        goodsname1: '黑鲨游戏手机 2',
-        goodsspecial1: '晓龙855，立体触控',
-        goodsprice1: '2899元起',
-        url2: '/images/index/mbipone/xiaomi9SE.png',
-        goodsname2: '小米9 SE',
-        goodsspecial2: '索尼4800万三摄，晓龙712',
-        goodsprice2: '1999元起',
+        url1: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/d039548049ef8bf66e5df4061851507a.jpg?w=1080&h=1080',
+        goodsname1: 'Redmi Note 7',
+        goodsspecial1: '4800万拍照 长续航',
+        goodsprice1: '999元起',
+        url2: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/5449b86700b536a8bb74ebdc4b20769c.jpg?w=1080&h=1080',
+        goodsname2: '小米9',
+        goodsspecial2: '骁龙855，索尼4800万三摄',
+        goodsprice2: '2999元起',
       }
     ],
   },
